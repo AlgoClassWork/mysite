af6be84cfd93eeee565d501bebdbb5f5eb3fdf8a
AlgoClassWork <kyxec.play@gmail.com>
2025-03-29 13:42:38 +0600

    Add files via upload

---
+++ ads/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from .models import Ad
+
+# Create your views here.
+def ad_list(request):
+    ads = Ad.objects.all() 
+    return render(request, 'ads/ad_list.html', {'ads':ads})
